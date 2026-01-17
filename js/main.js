e1b20dfdc1d45feb00a6f000e4581adbe69e895e
Akacz123 <100wiktor@gmail.com>
2026-01-17 22:26:17 +0100

    Stworzenie czarnego prostąkata czyli planszy po której będzie chodzil wąż

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 const canvas = document.getElementById("gameBoard");
 const ctx = canvas.getContext("2d");
+canvas.height = 600;
+canvas.width = 800;
+
+function gameLoop() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(gameLoop);
+}
+gameLoop();
