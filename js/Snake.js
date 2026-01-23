a987993e04e14d79306d6b861cb0a19225adf9ac
Akacz123 <100wiktor@gmail.com>
2026-01-23 15:53:41 +0100

    Powoli snake się pojawia i poprawka w html

--- js/Snake.js
+++ js/Snake.js
@@ -1,5 +1,13 @@
 class Snake {
   constructor(start) {
-    this.body = start;
+    this.body = [start];
+    this.direction = { x: 0, y: 0 };
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = "lime";
+    this.body.forEach((segment) => {
+      ctx.fillRect(segment.x * 20, segment.y * 20, 20, 20);
+    });
   }
 }
--- js/main.js
+++ js/main.js
@@ -3,9 +3,12 @@ const ctx = canvas.getContext("2d");
 canvas.height = 600;
 canvas.width = 800;
 
+const snake = new Snake({ x: 10, y: 10 });
+
 function gameLoop() {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  snake.draw(ctx);
   requestAnimationFrame(gameLoop);
 }
 gameLoop();
